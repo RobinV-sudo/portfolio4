21ca88a0bd8a67acbd575c2add6dd7b5f5e5b8a9
robin vorstedt robinvorstedt@outlook.com <robinvorstedt@outlook.com>
2025-05-08 18:29:09 +0900

    gem and build-up ready

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+# config/initializers/cloudinary.rb
+
+Cloudinary.config do |config|
+  config.cloud_name = 'dqdehka86'
+  config.api_key = '236457668671725'
+  config.api_secret = '6VRS2gOEhtPYJnUpA_JoMj1-4og'
+end
